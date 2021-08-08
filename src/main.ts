153815c2784354f1aa07da002fba20bee653b782
phibr0 <bronzel.phillip@gmail.com>
2021-08-08 21:21:30 +0200

    Change el.innerText to el.innerHTML so that other Markdown Formats don't break

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { emoji } from "./emojis"
 
 export default class EmojiShortcodesPlugin extends Plugin {
 	static emojiProcessor: MarkdownPostProcessor = (el: HTMLElement) => {
-		el.innerText.match(/:[^ \n]*:/g)?.forEach((e: keyof typeof emoji) => el.innerText = el.innerText.replace(e, emoji[e] ?? e));
+		el.innerText.match(/:[^ \n]*:/g)?.forEach((e: keyof typeof emoji) => el.innerHTML = el.innerHTML.replace(e, emoji[e] ?? e));
 	}
 
 	async onload() {
