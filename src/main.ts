28dfc64c8761b6bfecb67a7253aff68f2a55f254
phibr0 <bronzel.phillip@gmail.com>
2021-08-12 11:52:20 +0200

    implement recommendations from code review

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { MarkdownPostProcessor, Plugin } from 'obsidian';
 import { emoji } from "./emojis"
 
 export default class EmojiShortcodesPlugin extends Plugin {
-	static emojiProcessor: MarkdownPostProcessor = (el: HTMLElement) => {
-		el.innerText.match(/:[^ \n]*:/g)?.forEach((e: keyof typeof emoji) => el.innerHTML = el.innerHTML.replace(e, emoji[e] ?? e));
+	emojiProcessor: MarkdownPostProcessor = (el: HTMLElement) => {
+		el.innerText.match(/:[^ \n]*:/g)?.forEach((e: keyof typeof emoji) => this.emojiReplace(e, el));
 	}
 
-	async onload() {
-		this.registerMarkdownPostProcessor(EmojiShortcodesPlugin.emojiProcessor);
+	emojiReplace(shortcode: keyof typeof emoji, el: HTMLElement | ChildNode){
+		if (el.hasChildNodes()){
+			el.childNodes.forEach((child: ChildNode) => this.emojiReplace(shortcode, child));
+		} else {
+			el.textContent = el.textContent.replace(shortcode, emoji[shortcode] ?? shortcode);
+		}
+	}
+
+	onload() {
+		this.registerMarkdownPostProcessor(this.emojiProcessor);
 	}
 }
 
