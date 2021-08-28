27d8ba496025dce5a1be52586635fe1ac4d143fc
thxnder <36465489+tswwe@users.noreply.github.com>
2021-08-28 21:05:47 +0800

    Fix Bug#7 in Issus#13 (for Preview mode)
    Add a condition determined by checkForInputBlock( ) function to the emojiReplace( ) function, to fix Bug#7 for Preview mode.

--- src/markdownPostProcessor.ts
+++ src/markdownPostProcessor.ts
@@ -8,6 +8,9 @@ export default class EmojiMarkdownPostProcessor {
 	}
 
 	static emojiReplace(shortcode: keyof typeof emoji, el: HTMLElement | ChildNode){
+		if ((typeof(el.tagName)==="string") && (el.tagName.indexOf("CODE") !== -1 || el.tagName.indexOf("MJX") !== -1)) {
+			return false;
+		}
 		if (el.hasChildNodes()){
 			el.childNodes.forEach((child: ChildNode) => this.emojiReplace(shortcode, child));
 		} else {
